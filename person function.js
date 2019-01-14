488cfd57ec1f42ed2ed37b319b59ebea2cf45970
Jacques Lavoie <46101717+Stormyisland@users.noreply.github.com>
2019-01-13 16:00:17 -0800

    person Function
    Key value pair snippit.

---
+++ person function.js
@@ -0,0 +1,9 @@
+function person(name, favColor){
+console.log("hello,my name is" + name + " and myfavorite color is " + favColor + ".");
+}
+var johnName = "Jom Door";
+var johnFavColor = "blurg";
+
+person("Jam Door","blurg");
+person("Insane Smith", "greebm");
+
